73de1820c0510e2e934abc03f5444647b94cdf0a
Maria Eduarda <20211ireinfint0010@ifba.edu.br>
2025-08-21 09:41:38 -0300

    arquivos at

--- src/db/DB.java
+++ src/db/DB.java
@@ -16,6 +16,65 @@ public class DB {
             try{
                 Properties props = loadProperties();
                 String url = props.getProperty("dburl");
+
+                conn = DriverManager.getConnection(url, props);
+
+            }
+            catch(SQLException e){
+
+                throw new DbException(e.getMessage());
+
+            }
+        }
+        return conn;
+    }
+
+    public static void closeConnection() {
+        if (conn != null) {
+            try{
+                conn.close();
+
+            }
+            catch(SQLException e){
+                throw new DbException(e.getMessage());
+
+            }
+        }
+    }
+
+    public static Properties loadProperties() {
+
+        try(FileInputStream fs = new FileInputStream("db.properties")){
+            Properties props = new Properties();
+
+            props.load(fs);
+
+            return props;
+        }
+        catch(IOException e){
+            throw new DbException(e.getMessage());
+        }
+    }
+
+    public static void closeStatement(Statement st) {
+
+        if (st != null) {
+            try{
+                st.close();
+            }
+            catch(SQLException e){
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs) {
+        if (rs != null) {
+            try{
+                rs.close();
+            }
+            catch(SQLException e){
+                throw new DbException(e.getMessage());
             }
         }
     }
---
+++ src/program/Program.java
@@ -0,0 +1,36 @@
+package program;
+
+import db.DB;
+import java.sql.*; //usar o connection, ResultSet...
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Connection conn = DB.getConnection(); //se conecta com o db(database: banco de dados)
+
+        Statement st = null; //prepara para a consulta
+        ResultSet rs = null; //resultado da tabela
+
+        try {
+            conn = DB.getConnection();
+            st = conn.createStatement();
+            rs = st.executeQuery("select * from seller");
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("Id" + " " + rs.getString("Name") + "\n" + rs.getString("Email") + "\n"));
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+
+        }
+        finally{
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+
+    }
+
+}
